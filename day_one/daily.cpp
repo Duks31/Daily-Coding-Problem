aff6000f3ef874dc0c1756dbf9fb4c4f7f8ba0b9
Duks31 <chidubemjan31@gmail.com>
2022-12-10 21:27:50 +0100

    day one

--- day_one/daily.cpp
+++ day_one/daily.cpp
@@ -1,1 +1,15 @@
-// 
+// Write a function that takes a natural number as input and returns the number of digits the input has.
+// Constraint: don't use any loops.
+
+#include <string>
+#include <iostream>
+
+int number_of_digits(int n) {
+    std::string str = std::to_string(n);
+    return str.length();
+}
+
+int main() {
+    std::cout << number_of_digits(1234) <<std::endl;
+}
+    
